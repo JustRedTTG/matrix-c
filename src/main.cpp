7acd1bf78e3c3915689ebb108276ecb7cff976f0
JustRedTTG <redtonehair@gmail.com>
2024-12-13 21:46:45 +0200

    remove a bunch of comments

--- src/main.cpp
+++ src/main.cpp
@@ -50,15 +50,15 @@ int main(const int argc, char *argv[]) {
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
         glVertexAttribPointer(
-           0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           3,                  // size
-           GL_FLOAT,           // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           nullptr            // array buffer offset
+           0,
+           3,
+           GL_FLOAT,
+           GL_FALSE,
+           0,
+           nullptr
         );
         // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         glDisableVertexAttribArray(0);
 
         rnd->swapBuffers();
